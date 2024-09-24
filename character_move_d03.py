95e59f8ad82de5491e277ecd3a4424d6b44147b9
EUM(ZenbookDuo) <undermilk12@gmail.com>
2024-09-24 12:12:16 +0900

    원 운동 구현 -완-

--- character_move_d03.py
+++ character_move_d03.py
@@ -12,9 +12,17 @@ boy = load_image('character.png')
 def run_circle():
     print('CIRCLE')
     
-    clear_canvas_now()
-    boy.draw_now(400,300)
-    delay(0.01)
+    r, cx, cy = 300, 800//2, 600//2
+    for degree in range(0, 360, 3):
+        
+        theta = math.radians(degree)
+        
+        x = r * math.cos(theta) + cx
+        y = r * math.sin(theta) + cy
+    
+        clear_canvas_now()
+        boy.draw_now(x,y)
+        delay(0.01)
 
 def run_rectangle():
     print('RECTANGLE')
