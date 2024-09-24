8515171c641df3ebd64e90ad6fb63f433f9ffa51
EUM(ZenbookDuo) <undermilk12@gmail.com>
2024-09-24 11:52:43 +0900

    pico2d 뼈대 구축

---
+++ character_move_d03.py
@@ -0,0 +1,24 @@
+import os
+import math
+from pico2d import *
+
+open_canvas()
+
+os.chdir('C:\\tuk_GitHub\\2DGP-Drill-03')
+
+grass = load_image('grass.png')
+character = load_image('character.png')
+
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
+
+while True:
+    run_circle()
+    run_rectangle()
+
+    break
